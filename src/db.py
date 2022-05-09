1591d485c8ce91482232e51a9bbf5d78252331eb
Clark <daprogrammer1@gmail.com>
2022-05-09 15:22:03 +0800

    secure password

--- src/db.py
+++ src/db.py
@@ -1,9 +1,18 @@
 from mysql.connector import connect, ProgrammingError
 from json import load
-# from mysql.connector.errorcode import (
-#   ER_SERVER_ISNT_AVAILABLE
-# )
 from click import secho
+from cryptography.fernet import Fernet 
+from os import path
+from mysql.connector.errorcode import ER_ACCESS_DENIED_ERROR
+
+if path.exists('./db_password.key'):
+  with open('./db_password.key', 'r') as k:
+    key = k.read().encode()
+    f = Fernet(key)
+else:
+  key = Fernet.generate_key()
+  with open('./db_password.key', 'w') as k:
+    k.write(key.decode())
 
 class Database:
   loginfile = open('login.json', 'r') 
@@ -11,19 +20,20 @@ class Database:
 
   def __init__(self):
     try:
-      global db
+      self.details['password'] = f.decrypt(self.details['password'].encode()).decode()
       self.db = connect(**self.details)
-    # except Error as err:
-      # if err.errno == ER_SERVER_ISNT_AVAILABLE:
-        # print("server offline")
+      secho("Database connected successfully", fg='bright_green')
+
+      self.db.close()
     except ProgrammingError as err:
-      raise err
-    secho("Database connected successfully", fg='bright_green')
-    self.db.close()
+      f.decrypt(self.details['password'].encode()).decode()
+      if err.errno == ER_ACCESS_DENIED_ERROR:
+        print("Wrong password or username")
+      else:
+        print(err)
+
+      # print(errorcod)
   
-
-
-
 
     # with open('schema.sql') as r:
     #   self.db.executescript(r.read())
